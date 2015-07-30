f745cf39c0f2483638f1fbda4d25c6affe503ab9
Will <zhenzhwh@cn.ibm.com>
2015-07-30 22:42:43 +0800

    渲染首页中的新闻中心

---
+++ SchoolWeb/src/drw/model/BaseModel.java
@@ -0,0 +1,15 @@
+package drw.model;
+
+import com.jfinal.plugin.activerecord.Model;
+@SuppressWarnings("rawtypes")
+public class BaseModel<M extends Model> extends Model<M> {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	
+	
+
+}
